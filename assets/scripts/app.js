8e793eda0f91747996cb2e69ab376d028aea769d
Raisul Showmin <46886381+rishowmin@users.noreply.github.com>
2019-12-10 23:56:08 +0600

    Add files via upload
    My first commit.

---
+++ assets/scripts/app.js
@@ -0,0 +1,151 @@
+$(document).ready(function () {
+    var flag = 0;
+    $("#main-service-inner-1").hover(function () {
+        if (flag == 0) {
+            $('#service-descrip-1').css('display', 'block');
+            $('#service-descrip-1').css('transform', 'rotateY(180deg)');
+            $('#service-cover-1').css('display', 'none');
+            flag = 1;
+        } else if (flag == 1) {
+            $('#service-descrip-1').css('display', 'none');
+            $('#service-cover-1').css('display', 'block');
+            flag = 0;
+        }
+    });
+});
+
+$(document).ready(function () {
+    var flag = 0;
+    $("#main-service-inner-2").hover(function () {
+        if (flag == 0) {
+            $('#service-descrip-2').css('display', 'block');
+            $('#service-descrip-2').css('transform', 'rotateY(180deg)');
+            $('#service-cover-2').css('display', 'none');
+            flag = 1;
+        } else if (flag == 1) {
+            $('#service-descrip-2').css('display', 'none');
+            $('#service-cover-2').css('display', 'block');
+            flag = 0;
+        }
+    });
+});
+
+$(document).ready(function () {
+    var flag = 0;
+    $("#main-service-inner-3").hover(function () {
+        if (flag == 0) {
+            $('#service-descrip-3').css('display', 'block');
+            $('#service-descrip-3').css('transform', 'rotateY(180deg)');
+            $('#service-cover-3').css('display', 'none');
+            flag = 1;
+        } else if (flag == 1) {
+            $('#service-descrip-3').css('display', 'none');
+            $('#service-cover-3').css('display', 'block');
+            flag = 0;
+        }
+    });
+});
+
+$(document).ready(function () {
+    var flag = 0;
+    $("#main-service-inner-4").hover(function () {
+        if (flag == 0) {
+            $('#service-descrip-4').css('display', 'block');
+            $('#service-descrip-4').css('transform', 'rotateY(180deg)');
+            $('#service-cover-4').css('display', 'none');
+            flag = 1;
+        } else if (flag == 1) {
+            $('#service-descrip-4').css('display', 'none');
+            $('#service-cover-4').css('display', 'block');
+            flag = 0;
+        }
+    });
+});
+
+$(document).ready(function () {
+    var flag = 0;
+    $("#main-service-inner-5").hover(function () {
+        if (flag == 0) {
+            $('#service-descrip-5').css('display', 'block');
+            $('#service-descrip-5').css('transform', 'rotateY(180deg)');
+            $('#service-cover-5').css('display', 'none');
+            flag = 1;
+        } else if (flag == 1) {
+            $('#service-descrip-5').css('display', 'none');
+            $('#service-cover-5').css('display', 'block');
+            flag = 0;
+        }
+    });
+});
+
+$(document).ready(function () {
+    var flag = 0;
+    $("#main-service-inner-6").hover(function () {
+        if (flag == 0) {
+            $('#service-descrip-6').css('display', 'block');
+            $('#service-descrip-6').css('transform', 'rotateY(180deg)');
+            $('#service-cover-6').css('display', 'none');
+            flag = 1;
+        } else if (flag == 1) {
+            $('#service-descrip-6').css('display', 'none');
+            $('#service-cover-6').css('display', 'block');
+            flag = 0;
+        }
+    });
+});
+
+$(document).ready(function () {
+    var flag = 0;
+    $("#main-service-inner-7").hover(function () {
+        if (flag == 0) {
+            $('#service-descrip-7').css('display', 'block');
+            $('#service-descrip-7').css('transform', 'rotateY(180deg)');
+            $('#service-cover-7').css('display', 'none');
+            flag = 1;
+        } else if (flag == 1) {
+            $('#service-descrip-7').css('display', 'none');
+            $('#service-cover-7').css('display', 'block');
+            flag = 0;
+        }
+    });
+});
+
+$(document).ready(function () {
+    var flag = 0;
+    $("#main-service-inner-8").hover(function () {
+        if (flag == 0) {
+            $('#service-descrip-8').css('display', 'block');
+            $('#service-descrip-8').css('transform', 'rotateY(180deg)');
+            $('#service-cover-8').css('display', 'none');
+            flag = 1;
+        } else if (flag == 1) {
+            $('#service-descrip-8').css('display', 'none');
+            $('#service-cover-8').css('display', 'block');
+            flag = 0;
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    var flag = 0;
+    $("#staff-details").hover(function () {
+        if (flag == 0) {
+            $('#inner-staff-details').css('display', 'block');
+            flag = 1;
+        } else if (flag == 1) {
+            $('#inner-staff-details').css('display', 'none');
+            flag = 0;
+        }
+    });
+});
